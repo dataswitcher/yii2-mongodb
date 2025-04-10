ff5bfadaa4162cadc9f0beb2f176826dc9d5efb4
josedataswitcher <108273259+josedataswitcher@users.noreply.github.com>
2025-04-10 12:01:58 +0100

    Propagating the fix for the getId() method to BaseQueryResult class

--- src/BatchQueryResult.php
+++ src/BatchQueryResult.php
@@ -130,7 +130,15 @@ class BatchQueryResult extends BaseObject implements \Iterator
         if ($this->_iterator === null) {
             $this->query->addOptions(['batchSize' => $this->batchSize]);
             $cursor = $this->query->buildCursor($this->db);
-            $token = 'fetch cursor id = ' . $cursor->getId(true);
+
+            try {
+                # MongoDB >= 1.20
+                $token = 'fetch cursor id = ' . $cursor->getId(true);
+            } catch (\ArgumentCountError $e) {
+                # MongoDB < 1.20
+                $token = 'fetch cursor id = ' . $cursor->getId();
+            }
+
             Yii::info($token, __METHOD__);
 
             if ($cursor instanceof \Iterator) {
